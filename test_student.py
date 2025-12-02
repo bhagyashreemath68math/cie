5d3bc09ec51408f05780148929ce8c7a5801004f
bhagyashreemath68math <bhagyashreemath68@gmail.com>
2025-12-02 12:09:11 +0530

    Add test for student registration details function

---
+++ test_student.py
@@ -0,0 +1,12 @@
+from student import student_registration_details
+
+def test_student_details():
+
+    expected_output = (
+        "Student ID:323\n"
+        "Student Name:Bhagya\n"
+        "Course:BCA\n"
+        "Academic Year:2024-2025"
+    )
+
+    assert student_registration_details("323", "Bhagya", "BCA", "2024-2025") == expected_output
