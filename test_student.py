9bf7bf71b603bfeaedd8a6fb6cd99eb17dd94267
bhagyashreemath68math <bhagyashreemath68@gmail.com>
2025-12-02 12:40:35 +0530

    Fix formatting of assertion in test_student.py

--- test_student.py
+++ test_student.py
@@ -7,4 +7,4 @@ def test_student_details():
         "Course:BCA\n"
         "Academic Year:2024-2025"
     )
-    assert student_registration_details("323","Bhagya","BCA","2024-2025") == expected_output
+    assert student_registration_details("323","Bhagya","BCA","2024-2025")==expected_output
